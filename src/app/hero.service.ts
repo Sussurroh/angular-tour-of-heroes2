dddcf0fb7d046739e8bea576cee792658440c3e9
Sussurroh <igo.ramalho.atomicbomb@gmail.com>
2021-03-09 12:49:16 +0000

    4. Add Services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,15 @@ import { Observable, of } from "rxjs";
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
 
 @Injectable()
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add("HeroService: Fetched heroes")
     return heroes;
   }
 
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 }
